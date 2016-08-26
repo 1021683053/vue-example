f1206ad1c5cf340e20ffb7d1b127ed7a2bbc6505
Liweifeng <1021683053@qq.com>
2016-08-26 16:16:03 +0800

    aa

---
+++ vuex-counter/src/Counter.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <Display></Display>
+        <Increment></Increment>
+    </div>
+</template>
+<script>
+import Display from './Display.vue';
+import Increment from './Increment.vue';
+import store from './store';
+
+export default{
+    components: {
+        Display: Display,
+        Increment: Increment
+    },
+    store: store
+}
+</script>
---
+++ vuex-counter/src/Display.vue
@@ -0,0 +1,18 @@
+<template >
+    <div>
+        <h3>Count is :{{count}}</h3>
+    </div>
+</template>
+
+<script>
+// import { getCount } from './getter';
+export default {
+    vuex: {
+        getters: {
+            count: function(state){
+                return state.count;
+            }
+        }
+    }
+};
+</script>
