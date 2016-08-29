9955ee1c5293633f590444b69fdc236c87b82a91
Liweifeng <1021683053@qq.com>
2016-08-29 18:01:28 +0800

    twat

---
+++ vue-cli/src/Index.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="context">
+
+        <div class="group">
+            <ui-toolbar
+                type="colored" text-color="white" brand="APP" title="Inbox"
+                show-brand
+            >
+                <div slot="actions">
+                    <ui-icon-button
+                        type="clear" color="white" icon="arrow_back"
+                    ></ui-icon-button>
+
+                    <ui-icon-button
+                        type="clear" color="white" icon="star_border"
+                    ></ui-icon-button>
+
+                    <ui-icon-button
+                        type="clear" color="white" icon="more_vert" has-dropdown-menu
+                        :menu-options="menu" dropdown-position="bottom right"
+                    ></ui-icon-button>
+                </div>
+            </ui-toolbar>
+        </div>
+
+    </div>
+
+</template>
+
+<script>
+import { UiButton, UiTab, UiTabs, UiToolbar, UiIconButton } from 'keen-ui';
+export default {
+    data() {
+        return {
+            menu: [{
+                id: 'settings',
+                text: 'Settings'
+            }, {
+                id: 'about',
+                text: 'About'
+            }, {
+                id: 'help',
+                text: 'Help'
+            }]
+        };
+    },
+    computed: {},
+    ready() {},
+    attached() {},
+    methods: {},
+    components: {
+        UiButton,
+        UiTab,
+        UiTabs,
+        UiToolbar,
+        UiIconButton
+    }
+};
+</script>
+
+<style>
+    .context{
+        height: 200px;
+    }
+</style>
--- vue-cli/src/main.js
+++ vue-cli/src/main.js
@@ -1,8 +1,11 @@
-import Vue from 'vue'
-import App from './App'
-
-/* eslint-disable no-new */
+import Vue from 'vue';
+require('./assets/base.css');
+require('keen-ui/dist/keen-ui.css');
+// require('')
+import Index from './Index.vue';
 new Vue({
-  el: 'body',
-  components: { App }
-})
+    el: 'body',
+    components: {
+        Index
+    }
+});
